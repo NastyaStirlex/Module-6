5ee0b5ecb1301f04724815de062e459403b0343c
PolinaBaranova21 <101976325+PolinaBaranova21@users.noreply.github.com>
2022-03-31 12:42:56 +0300

    Add files via upload

---
+++ Matrix.h
@@ -0,0 +1,16 @@
+#pragma once
+class Matrix
+{
+	double** matrix;
+	int row, col;
+public:
+	void Init(int row, int col);
+	void Rand();
+	static void Multi(const Matrix& m,const double* b,int n,double*c);
+	static void Multi_T(const Matrix& m, const double* b, int n, double* c);//умнжение транспонированной матрицы
+	static void SumVector(double* a,const double* b,int n);
+	double& operator()(int i, int j);//пергрузка оператора()
+	friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
+	friend std::istream& operator>>(std::istream& is,  Matrix& m);
+};
+
