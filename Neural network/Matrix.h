c8bf22e2f201af9f3e84ad22c2f0a4ded9025660
PolinaBaranova21 <101976325+PolinaBaranova21@users.noreply.github.com>
2022-04-07 18:14:48 +0300

    Update Matrix.h

--- Neural network/Matrix.h
+++ Neural network/Matrix.h
@@ -7,10 +7,10 @@ class Matrix
 public:
 	void Init(int row, int col);
 	void Rand();
-	static void Multi(const Matrix& m, const double* b, int n, double* c);
-	static void Multi_T(const Matrix& m, const double* b, int n, double* c);//умнжение транспонированной матрицы
-	static void SumVector(double* a, const double* b, int n);
-	double& operator ()(int i, int j);//перегрузка оператора()
+	static void Multi(const Matrix& m,const double* b,int n,double*c);
+	static void Multi_T(const Matrix& m, const double* b, int n, double* c);//СѓРјРЅР¶РµРЅРёРµ С‚СЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРЅРѕР№ РјР°С‚СЂРёС†С‹
+	static void SumVector(double* a,const double* b,int n);
+	double& operator()(int i, int j);//РїРµСЂРіСЂСѓР·РєР° РѕРїРµСЂР°С‚РѕСЂР°()
 	friend std::ostream& operator<<(std::ostream& os, const Matrix& m);
 	friend std::istream& operator>>(std::istream& is,  Matrix& m);
 };
