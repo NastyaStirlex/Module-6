79e6cf4be61513685b0541288b3d17cddc76c7f1
NastyaStirlex <nastia.tikhonova.2002@mail.ru>
2022-04-08 17:07:38 +0700

    Убрала  std:: и поменяла местоположение некоторых структур и классов

--- Neural network/neyronka.cpp
+++ Neural network/neyronka.cpp
@@ -7,6 +7,11 @@ using namespace std; // из Network.h
 
 struct data_Network { int L; int* size; };
 
+struct data_info {
+    double* pixels;
+    int digit;
+};
+
 enum activateFunc { sigmoid = 1, ReLU, thx };
 
 class ActivateFunction {
@@ -18,6 +23,104 @@ public:
     double useDer(double value);
 };
 
+void ActivateFunction::set() {
+    cout << "Set act Func pls\n1 - sigmoid \n2 - ReLU \n3 - th(x)\n";
+    int temp;
+    cin >> temp;
+    switch (temp) {
+    case sigmoid:
+        actFunc = sigmoid;
+        break;
+    case ReLU:
+        actFunc = ReLU;
+        break;
+    case thx:
+        actFunc = thx;
+        break;
+    default:
+        throw runtime_error("Error read actFunc"); //выбрасываем исключения программа останавливается
+        break;
+
+    }
+}
+
+void ActivateFunction::use(double* value, int n) { //формулы
+    switch (actFunc) {
+    case activateFunc::sigmoid:
+        for (int i = 0; i < n; i++)
+            value[i] = 1 / (1 + exp(-value[i]));
+        break;
+    case activateFunc::ReLU:
+        for (int i = 0; i < n; i++) {
+            if (value[i] < 0)
+                value[i] *= 0.01;
+            else if (value[i] > 1)
+                value[i] = 1. + 0.01 * (value[i] - 1.);//else value=value
+        }
+        break;
+    case activateFunc::thx:
+        for (int i = 0; i < n; i++) {
+            if (value[i] < 0)
+                value[i] = 0.01 * (exp(value[i]) - exp(-value[i])) / (exp(value[i]) + exp(-value[i]));
+            else
+                value[i] = (exp(value[i]) - exp(-value[i])) / (exp(value[i]) + exp(-value[i]));
+        }
+        break;
+    default:
+        throw runtime_error("Error actFunc \n");
+        break;
+    }
+}
+
+void ActivateFunction::useDer(double* value, int n) {
+    switch (actFunc) {
+    case activateFunc::sigmoid:
+        for (int i = 0; i < n; i++)
+            value[i] = value[i] * (1 - value[i]);
+        break;
+    case activateFunc::ReLU:
+        for (int i = 0; i < n; i++) {
+            if (value[i] < 0 || value[i] >1)
+                value[i] = 0.01;
+            else
+                value[i] = 1;
+        }
+        break;
+    case activateFunc::thx:
+        for (int i = 0; i < n; i++) {
+            if (value[i] < 0)
+                value[i] = 0.01 * (1 - value[i] * value[i]);
+            else
+                value[i] = 1 - value[i] * value[i];
+        }
+        break;
+    default:
+        throw runtime_error("Error actFuncDer \n");
+        break;
+    }
+}
+
+double ActivateFunction::useDer(double value) {
+    switch (actFunc) {
+    case activateFunc::sigmoid:
+        value = 1 / (1 + exp(-value));
+        break;
+    case activateFunc::ReLU:
+        if (value < 0 || value >1)
+            value = 0.01;
+        break;
+    case activateFunc::thx:
+        if (value < 0)
+            value = 0.01 * (exp(value) - exp(-value)) / (exp(value) + exp(-value));
+        else
+            value = (exp(value) - exp(-value)) / (exp(value) + exp(-value));
+        break;
+    default:
+        throw runtime_error("Error actFuncDer \n");
+        break;
+    }
+    return value;
+}
 
 class Network {
     int L; //слои
@@ -53,114 +156,8 @@ public:
     static void Multi_T(const Matrix& m, const double* b, int n, double* c); //умножение транспонированной матрицы
     static void SumVector(double* a, const double* b, int n);
     double& operator()(int i, int j); //перегрузка оператора()
-    friend std::ostream& operator << (std::ostream& os, const Matrix& m);
-    friend std::istream& operator >> (std::istream& is, Matrix& m);
-};
-
-
-
-void ActivateFunction::set() {
-    std::cout << "Set act Func pls\n1 - sigmoid \n2 - ReLU \n3 - th(x)\n";
-    int temp;
-    std::cin >> temp;
-    switch (temp) {
-    case sigmoid:
-        actFunc = sigmoid;
-        break;
-    case ReLU:
-        actFunc = ReLU;
-        break;
-    case thx:
-        actFunc = thx;
-        break;
-    default:
-        throw std::runtime_error("Error read actFunc"); //выбрасываем исключения программа останавливается
-        break;
-
-    }
-}
-
-void ActivateFunction::use(double* value, int n) { //формулы
-    switch (actFunc) {
-    case activateFunc::sigmoid:
-        for (int i = 0; i < n; i++)
-            value[i] = 1 / (1 + exp(-value[i]));
-        break;
-    case activateFunc::ReLU:
-        for (int i = 0; i < n; i++) {
-            if (value[i] < 0)
-                value[i] *= 0.01;
-            else if (value[i] > 1)
-                value[i] = 1. + 0.01 * (value[i] - 1.);//else value=value
-        }
-        break;
-    case activateFunc::thx:
-        for (int i = 0; i < n; i++) {
-            if (value[i] < 0)
-                value[i] = 0.01 * (exp(value[i]) - exp(-value[i])) / (exp(value[i]) + exp(-value[i]));
-            else
-                value[i] = (exp(value[i]) - exp(-value[i])) / (exp(value[i]) + exp(-value[i]));
-        }
-        break;
-    default:
-        throw std::runtime_error("Error actFunc \n");
-        break;
-    }
-}
-
-void ActivateFunction::useDer(double* value, int n) {
-    switch (actFunc) {
-    case activateFunc::sigmoid:
-        for (int i = 0; i < n; i++)
-            value[i] = value[i] * (1 - value[i]);
-        break;
-    case activateFunc::ReLU:
-        for (int i = 0; i < n; i++) {
-            if (value[i] < 0 || value[i] >1)
-                value[i] = 0.01;
-            else
-                value[i] = 1;
-        }
-        break;
-    case activateFunc::thx:
-        for (int i = 0; i < n; i++) {
-            if (value[i] < 0)
-                value[i] = 0.01 * (1 - value[i] * value[i]);
-            else
-                value[i] = 1 - value[i] * value[i];
-        }
-        break;
-    default:
-        throw std::runtime_error("Error actFuncDer \n");
-        break;
-    }
-}
-
-double ActivateFunction::useDer(double value) {
-    switch (actFunc) {
-    case activateFunc::sigmoid:
-        value = 1 / (1 + exp(-value));
-        break;
-    case activateFunc::ReLU:
-        if (value < 0 || value >1)
-            value = 0.01;
-        break;
-    case activateFunc::thx:
-        if (value < 0)
-            value = 0.01 * (exp(value) - exp(-value)) / (exp(value) + exp(-value));
-        else
-            value = (exp(value) - exp(-value)) / (exp(value) + exp(-value));
-        break;
-    default:
-        throw std::runtime_error("Error actFuncDer \n");
-        break;
-    }
-    return value;
-}
-
-struct data_info {
-    double* pixels;
-    int digit;
+    friend ostream& operator << (ostream& os, const Matrix& m);
+    friend istream& operator >> (istream& is, Matrix& m);
 };
 
 data_Network ReadDataNetWork(string path) {
@@ -252,6 +249,7 @@ void Network::Init(data_Network data) {
     for (int i = 0; i < L - 1; i++)
         neurons_bios_val[i] = 1;
 }
+
 void Network::PrintConfig() {
     cout << "***********************************************************\n";
     cout << "Network has " << L << " layers\nSIZE[]: ";
@@ -260,6 +258,7 @@ void Network::PrintConfig() {
     }
     cout << "\n***********************************************************\n\n";
 }
+
 void Network::SetInput(double* values) {
     for (int i = 0; i < size[0]; i++) {
         neurons_val[0][i] = values[i];
@@ -275,6 +274,7 @@ double Network::ForwardFeed() {
     int pred = SearchMaxIndex(neurons_val[L - 1]);
     return pred;
 }
+
 int Network::SearchMaxIndex(double* value) {
     double max = value[0];
     int prediction = 0;
@@ -385,7 +385,7 @@ void Matrix::Rand() {
 
 void Matrix::Multi(const Matrix& m1, const double* neuron, int n, double* c) {
     if (m1.col != n)
-        throw std::runtime_error("Error Multi \n"); //iskluchenia
+        throw runtime_error("Error Multi \n"); //iskluchenia
     for (int i = 0; i < m1.row; ++i) {
         double tmp = 0;
         for (int j = 0; j < m1.col; ++j) {
@@ -397,7 +397,7 @@ void Matrix::Multi(const Matrix& m1, const double* neuron, int n, double* c) {
 
 void Matrix::Multi_T(const Matrix& m1, const double* neuron, int n, double* c) {
     if (m1.row != n)
-        throw std::runtime_error("Error Multi \n"); //iskluchenia
+        throw runtime_error("Error Multi \n"); //iskluchenia
     for (int i = 0; i < m1.col; ++i) {
         double tmp = 0;
         for (int j = 0; j < m1.row; ++j) {
@@ -416,7 +416,7 @@ void Matrix::SumVector(double* a, const double* b, int n) {
         a[i] += b[i];
 }
 
-std::ostream& operator<<(std::ostream& os, const Matrix& m) {
+ostream& operator<<(ostream& os, const Matrix& m) {
     for (int i = 0; i < m.row; ++i) {
         for (int j = 0; j < m.col; j++) {
             os << m.matrix[i][j] << " ";
@@ -425,7 +425,7 @@ std::ostream& operator<<(std::ostream& os, const Matrix& m) {
     return os;
 }
 
-std::istream& operator >>(std::istream& is, Matrix& m) {
+istream& operator >>(istream& is, Matrix& m) {
     for (int i = 0; i < m.row; ++i) {
         for (int j = 0; j < m.col; j++) {
             is >> m.matrix[i][j];
@@ -505,5 +505,6 @@ int main() {
         cin >> repeat;
     }
     system("pause");
+
     return 0;
 }
